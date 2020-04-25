59bbbea8a901dc8411d4cf79545c50e39dc50fa6
jakubwirfel <jakubwirfel@gmail.com>
2020-04-25 11:45:35 +0200

    Add delete, update, create job listening

--- config/init.php
+++ config/init.php
@@ -1,6 +1,12 @@
 <?php
+// Start Session
+session_start();
+
 //Config file
 require_once 'config.php';
+
+//Include Helpers
+require_once 'helpers/system_helper.php';
 
 //Autoloader
 function __autoload($class_name) {
--- templates/frontpage.php
+++ templates/frontpage.php
@@ -21,7 +21,7 @@
             <p><?php echo $job -> description ?></p>
         </div>
         <div class="col-md-2">
-            <a class="btn btn-secondary" href="#">View</a>
+            <a class="btn btn-secondary" href="job.php?id=<?php echo $job ->id; ?>">View</a>
         </div>
     </div>
     <?php endforeach;?>
