532910fcf01342248a9c85d130d0ec62868834e5
jakubwirfel <jakubwirfel@gmail.com>
2020-04-25 09:37:02 +0200

    Job filter by category

--- index.php
+++ index.php
@@ -5,8 +5,17 @@
 
     $template = new Template('./templates/frontpage.php');
 
-    $template -> title = 'Latest Jobs';
-    $template -> jobs = $job -> getAllJobs();
+    $category = isset($_GET['category']) ? $_GET['category'] : null;
+
+    if($category) {
+        $template -> jobs = $job -> getByCategory($category);
+        $template -> title = 'Jobs In '. $job -> getCategory($category)->name;
+    } else {
+        $template -> title = 'Latest Jobs';
+        $template -> jobs = $job -> getAllJobs();
+    }
+
+    $template -> categories = $job -> getCategories();
 
     echo $template;
 ?>
